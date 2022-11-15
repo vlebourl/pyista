3864bae42060c13870ab4ee62fac219ff9aabdad
vlebourl <vlebourl@gmail.com>
2022-11-15 16:12:19 +0100

    initial commit.

---
+++ ista.py
@@ -0,0 +1,12 @@
+"""Ista Webconsole API."""
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Ista:
+    """Ista main class."""
+
+    def __init__(self) -> None:
+        """Initialize Ista class."""
