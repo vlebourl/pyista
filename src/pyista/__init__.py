72872f0f65370b10e71541c1be4049ddc64aba79
vlebourl <vlebourl@gmail.com>
2022-11-15 16:14:52 +0100

    fix paths

---
+++ src/pyista/__init__.py
@@ -0,0 +1,12 @@
+"""Ista Webconsole API."""
+
+# pylint: skip-file
+# flakes8: noqa
+import logging
+
+# from .ista import Ista
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)5s - %(message)s",
+)
